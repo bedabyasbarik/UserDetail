91f102dbb0de277d9a86f8ce87f1ae49a8fe37e0
BBarik <BBarik@A4AIGMD23877.mindtree_odc01.com>
2017-05-30 12:08:29 +0530

    address

---
+++ src/com/webser/vo/Address.java
@@ -0,0 +1,23 @@
+package com.webser.vo;
+
+public class Address {
+     public String street;
+     public String city;
+     
+     public Address(String street,String city){
+    	 this.street=street;
+    	 this.city=city;
+     }
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+}
